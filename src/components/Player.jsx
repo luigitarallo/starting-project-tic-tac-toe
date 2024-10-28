d9652dcd1ce73828f8d9e77cd87b6efcfdaceb51
luigitarallo <tarallo.gigi@gmail.com>
2024-10-28 10:57:27 +0100

    add condition to show Save and Edit to button and value to input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,7 +9,7 @@ export default function Player({ name, symbol }) {
   let playerName = <span className="player-name">{name}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
   }
   return (
     <li>
@@ -17,7 +17,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
