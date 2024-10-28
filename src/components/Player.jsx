e317772cfc45adf16d38de6cfa1acd6bbde04809
luigitarallo <tarallo.gigi@gmail.com>
2024-10-28 10:58:48 +0100

    improve handleEditClick with ternary to change state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(isEditing ? false : true);
   }
   let playerName = <span className="player-name">{name}</span>;
 
