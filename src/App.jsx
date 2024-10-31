aa4f6e75f5973162e04ec79287be775054c28f43
luigitarallo <tarallo.gigi@gmail.com>
2024-10-31 10:06:31 +0100

    add const hasDraw to accept not winner, pass prop to GameOver component

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
@@ -84,7 +86,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
